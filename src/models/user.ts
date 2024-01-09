f40da0f6a1aad74dd10a26e3d2647d6d125d8f00
JoseEstradaS <jose5642260@gmail.com>
2024-01-09 14:16:44 -0500

    chore: added user model

---
+++ src/models/user.ts
@@ -0,0 +1,19 @@
+import mongoose, { type Document, Schema } from 'mongoose'
+
+export interface UserDocument extends Document {
+  email: string
+  firstName: string
+  lastName: string
+  password: string
+  loginAttempts: number
+}
+
+const userSchema = new Schema<UserDocument>({
+  email: { type: String, required: true, unique: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  password: { type: String, required: true },
+  loginAttempts: { type: Number, default: 0 }
+})
+
+export default mongoose.model<UserDocument>('User', userSchema)
